9c7e3f4d7992716734716916d54a3a9912b85bf4
Jack Harter <121989267+jhartrallscripts@users.noreply.github.com>
2023-04-24 13:25:07 -0500

    NewTests02

--- .github/scripts/find_another_release_branch01.sh
+++ .github/scripts/find_another_release_branch01.sh
@@ -12,3 +12,27 @@ echo "::set-output name=current_branch::$current_branch"
 # The date is expected to be in the format YYYY-MM-DD.
 branches=`git branch -r --sort=-committerdate | grep -E "origin/release/[0-9]{4}-[0-9]{2}-[0-9]{2}"`
 echo "::set-output name=branches::$branches"
+
+# Sort the branches from newest to oldest per the date field in their name.
+sorted_branches=`echo "$branches" | sort -r`
+echo "::set-output name=sorted_branches::$sorted_branches"
+
+# Set a variable for the latest branch.
+latest_branch=`echo "$sorted_branches" | head -n 1 | sed -e 's/origin\///'`
+echo "::set-output name=latest_branch::$latest_branch"
+
+# Check if the current branch is the latest branch.
+if [[ "$current_branch" != "$latest_branch" ]]; then
+    echo "::set-output name=latest_branch::$latest_branch"
+else
+    echo "::set-output name=latest_branch::"
+fi
+
+# If the current branch is the latest branch, echo a message.
+if [[ "$current_branch" == "$latest_branch" ]]; then
+    echo "The current branch is the latest release branch."
+fi
+
+
+
+exit 0

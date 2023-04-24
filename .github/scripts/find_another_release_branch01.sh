6f6cf742199fa272c0651def62b3a40edeecce22
Jack Harter <121989267+jhartrallscripts@users.noreply.github.com>
2023-04-24 14:31:26 -0500

    NewTest13

--- .github/scripts/find_another_release_branch01.sh
+++ .github/scripts/find_another_release_branch01.sh
@@ -9,19 +9,19 @@ git fetch
 
 # Confirm the name of the current branch.
 current_branch=`git rev-parse --abbrev-ref HEAD`
-echo "::set-output name=current_branch::$current_branch"
+echo "The current branch is $current_branch"
 
 # Get the list of all release branches, sorted by date number in the branch name.
 # The date is expected to be in the format YYYY-MM-DD.
 branches=`git branch -r --sort=-committerdate | grep -E "origin/release/[0-9]{4}-[0-9]{2}-[0-9]{2}"`
-echo "::set-output name=branches::$branches"
+echo "Extant branches are as follows: $branches"
 
 # if the current branch is has an earlier date in its name than the latest release branch, then it is not the latest release branch.
 # In this case, we want to output the name of the latest release branch.
 # If the current branch is the latest release branch, then we want to output an empty string.
 
 latest_branch=`echo "$branches" | head -n 1 | sed -e 's/origin\///'`
-echo "::set-output name=latest_branch::$latest_branch"
+echo "The latest branch is $latest_branch"
 
 # output the name of the latest release branch.
 if [[ "$current_branch" != "$latest_branch" ]]; then

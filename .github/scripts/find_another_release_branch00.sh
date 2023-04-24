453e2ae999a7d573a9499def0909a2bfa9b00690
Jack Harter <121989267+jhartrallscripts@users.noreply.github.com>
2023-04-24 12:18:21 -0500

    newTest01

--- .github/scripts/find_another_release_branch00.sh
+++ .github/scripts/find_another_release_branch00.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 
 # This script is used to find another release branch with a later date than the current one.
 # It is used to determine if the current branch is the latest release branch.
@@ -11,10 +11,9 @@ set -e
 current_branch=`git rev-parse --abbrev-ref HEAD`
 echo "::set-output name=current_branch::$current_branch"
 
-# Get the list of release branches, sorted by dates in their titles.
+# Get the list of all release branches, sorted by date number in the branch name.
 # The date is expected to be in the format YYYY-MM-DD.
-
-branches=`git branch -r --sort=-committerdate | grep -E "origin/release-[0-9]{4}-[0-9]{2}-[0-9]{2}"`
+branches=`git branch -r --sort=-committerdate | grep -E "origin/release/[0-9]{4}-[0-9]{2}-[0-9]{2}"`
 echo "::set-output name=branches::$branches"
 
 # if the current branch is has an earlier date in its name than the latest release branch, then it is not the latest release branch.
---
+++ .github/scripts/find_another_release_branch01.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script is used to check if there is a relase branch with a later date than the current one in its name.
+# If there is a later release branch, then the current branch is not the latest release branch.
+# If there is not a later release branch, then the current branch is the latest release branch.
+
+# Confirm the name of the current branch.
+current_branch=`git rev-parse --abbrev-ref HEAD`
+echo "::set-output name=current_branch::$current_branch"
+
+# Get the list of all release branches, sorted by date number in the branch name.
+# The date is expected to be in the format YYYY-MM-DD.
+branches=`git branch -r --sort=-committerdate | grep -E "origin/release/[0-9]{4}-[0-9]{2}-[0-9]{2}"`
+echo "::set-output name=branches::$branches"

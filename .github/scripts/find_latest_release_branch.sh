3751226d790fb7d03e8a3fcf46944abe0238ae0f
Jack Harter <121989267+jhartrallscripts@users.noreply.github.com>
2023-04-25 10:14:33 -0500

    NewTest26

--- .github/scripts/find_latest_release_branch.sh
+++ .github/scripts/find_latest_release_branch.sh
@@ -14,19 +14,17 @@ echo "The current branch is $current_branch"
 # Get the list of all release branches, sorted by date number in the branch name.
 # The date is expected to be in the format YYYY-MM-DD.
 branches=`git branch -r --sort=-committerdate | grep -E "origin/release/[0-9]{4}-[0-9]{2}-[0-9]{2}"`
-echo "Extant branches are as follows: $branches"
 
 # sort the list of branches by date number in the branch name.
 # The date is expected to be in the format YYYY-MM-DD.
 sorted_branches=`echo "$branches" | sort -r`
-echo "Sorted branches are as follows: $sorted_branches"
 
 # Get the name of the latest release branch
 latest_branch=`echo "$sorted_branches" | head -n 1 | sed 's/origin\///g'`
 echo "The latest branch is $latest_branch"
 
 # Now that we have the latest branch, we set it to the output variable.
-echo "$latest_branch=latest_branch" >> $GITHUB_OUTPUT
+echo "latest_branch=$latest_branch" >> $GITHUB_OUTPUT
 
 # Good work, go have a beer:
 exit 0
